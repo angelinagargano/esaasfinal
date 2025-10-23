ad6a2012a2a35d6a49cc4004aee2ac95b5957f38
LeanaNgompe <jlngompe@gmail.com>
2025-10-23 13:22:17 -0400

    copied from the rotton patatoes intro

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,63 @@
+class MoviesController < ApplicationController
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    @all_ratings = Movie.all_ratings
+    if params[:ratings]
+      @ratings_to_show = params[:ratings].keys || @all_ratings
+      session[:ratings] = params[:ratings]
+    else
+      @ratings_to_show = session[:ratings]&.keys || @all_ratings
+    end
+    if params[:sort_by]
+      @sort_by = params[:sort_by]
+      session[:sort_by] = params[:sort_by]
+    else 
+      @sort_by = session[:sort_by]
+    end 
+
+    @movies = Movie.where(rating: @ratings_to_show)
+    @movies = @movies.order(@sort_by) if @sort_by.present?
+
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+  private
+  # Making "internal" methods private is not required, but is a common practice.
+  # This helps make clear which methods respond to requests, and which ones do not.
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+end
