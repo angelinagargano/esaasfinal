a4a0675d038f66a96eb6ee8757f5f74d64de8fb6
LeanaNgompe <jlngompe@gmail.com>
2025-11-27 09:43:29 -0500

    users are able to accept, reject and unfriend other users.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -17,33 +17,42 @@ class FriendshipsController < ApplicationController
     end
     #accepting friend request
     def accept 
-        @friendship = current_user.inverse_friendships.find_by(user_id: params[:id])
+        friend_id = params[:friend_id] || params[:id]
+        @friendship = current_user.inverse_friendships.find_by(user_id: friend_id)
         if @friendship
-            @friendship.update(status: true)
-            flash[:notice] = "Friend request accepted."
+            if @friendship.update(status: true)
+                flash[:notice] = "Friend request accepted."
+            else
+                flash[:alert] = "Unable to accept friend request."
+            end
         else
             flash[:alert] = "Unable to accept friend request."
         end
+        redirect_back(fallback_location: user_profile_path(current_user))
     end 
     #rejecting friend request 
     def reject
-        @friendship = current_user.inverse_friendships.find_by(user_id: params[:id])
+        friend_id = params[:friend_id] || params[:id]
+        @friendship = current_user.inverse_friendships.find_by(user_id: friend_id)
         if @friendship
             @friendship.destroy
             flash[:notice] = "Friend request rejected."
         else
             flash[:alert] = "Unable to reject friend request."
         end
+        redirect_back(fallback_location: user_profile_path(current_user))
     end
     # unfriending a friend
     def unfriend
-        @friendship = current_user.friendships.find_by(friend_id: params[:id]) || current_user.inverse_friendships.find_by(user_id: params[:id])
+        friend_id = params[:friend_id] || params[:id]
+        @friendship = current_user.friendships.find_by(friend_id: friend_id) || current_user.inverse_friendships.find_by(user_id: friend_id)
         if @friendship
             @friendship.destroy
             flash[:notice] = "Unfriended successfully."
         else
             flash[:alert] = "Unable to unfriend."
         end
+        redirect_back(fallback_location: user_profile_path(current_user))
     end
 
 end
